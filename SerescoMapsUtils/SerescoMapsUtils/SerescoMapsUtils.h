b6c18421a1f1431a7c3876cfd53ff3e3d53564c5
diego <92336377+diegosalcedov@users.noreply.github.com>
2022-08-17 00:32:34 -0500

    first

---
+++ SerescoMapsUtils/SerescoMapsUtils/SerescoMapsUtils.h
@@ -0,0 +1,18 @@
+//
+//  SerescoMapsUtils.h
+//  SerescoMapsUtils
+//
+//  Created by diegitsen on 17/08/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SerescoMapsUtils.
+FOUNDATION_EXPORT double SerescoMapsUtilsVersionNumber;
+
+//! Project version string for SerescoMapsUtils.
+FOUNDATION_EXPORT const unsigned char SerescoMapsUtilsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SerescoMapsUtils/PublicHeader.h>
+
+
